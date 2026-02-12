2cceca9844fe9746412e0c5291cab12eb3e17d17
Ellin <me@ellin.co>
2026-02-12 20:25:59 +0100

    Switch CLI downloads from tar.gz to zip for notarization

--- Formula/subline.rb
+++ Formula/subline.rb
@@ -6,12 +6,12 @@ class Subline < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/fenelon/subline-go/releases/download/#{version}/subline-darwin-arm64.tar.gz"
+      url "https://github.com/fenelon/subline-go/releases/download/#{version}/subline-darwin-arm64.zip"
       sha256 "3625d7a2bb580d89739b697a490a2bccaea81297b20d05f4d94217796d692915"
     end
 
     on_intel do
-      url "https://github.com/fenelon/subline-go/releases/download/#{version}/subline-darwin-amd64.tar.gz"
+      url "https://github.com/fenelon/subline-go/releases/download/#{version}/subline-darwin-amd64.zip"
       sha256 "704be9f93d4cbedc5158ec8f10805a47232642e1d6847e5ebc32a6e23456e2ad"
     end
   end
