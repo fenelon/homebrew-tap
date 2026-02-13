1981bc6250d9ecf128f66a64c1daa6690ab8647b
Ellin <me@ellin.co>
2026-02-13 23:50:45 +0100

    Update subline to 0.3.6

--- Casks/subline.rb
+++ Casks/subline.rb
@@ -1,8 +1,8 @@
 cask "subline" do
-  version "0.3.5"
-  sha256 "9a8b231478d509a317993e7ac48fb51c6fec1928493bc7007cfc2198dd183ad5"
+  version "0.3.6"
+  sha256 "ce2c61a004b6f00e2adc51ffc4cedd47eafabae571855532a63a039c1ca06741"
 
-  url "https://github.com/fenelon/subline-releases/releases/download/#{version}/subline-macos-app.zip"
+  url "https://github.com/fenelon/subline-releases/releases/download/#{version}/Subline-#{version}.dmg"
   name "Subline"
   desc "AI subtitles and transcripts made easy"
   homepage "https://github.com/fenelon/subline-releases"
--- Formula/subline.rb
+++ Formula/subline.rb
@@ -1,18 +1,18 @@
 class Subline < Formula
   desc "AI subtitles and transcripts made easy"
   homepage "https://github.com/fenelon/subline-releases"
-  version "0.3.5"
+  version "0.3.6"
   license :cannot_represent
 
   on_macos do
     on_arm do
       url "https://github.com/fenelon/subline-releases/releases/download/#{version}/subline-darwin-arm64.zip"
-      sha256 "6936a03e2b1e0ca62778b0f8556db1baf3f88878663dbce7c77845aeac6fd388"
+      sha256 "1eb3931b42d7541fbd560ee377edf259ca16019e4194931f269399fca2ecaeb0"
     end
 
     on_intel do
       url "https://github.com/fenelon/subline-releases/releases/download/#{version}/subline-darwin-amd64.zip"
-      sha256 "fb3013e4026114f803ab409570faedd43d42bda12273902f1a55bc27180d5e55"
+      sha256 "ab6f61d5bd6d4ff15dcbe0d36ddae48c8d09360ee53707d29fdc86c1be78d9cd"
     end
   end
 
